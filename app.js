d42689215d250cbd4b18acc4ab127b7aee4e3a92
IKO006 <iko0070605@gmail.com>
2023-05-24 16:11:09 +0600

    done hw 3

---
+++ app.js
@@ -0,0 +1,30 @@
+//var myNameIs = "Iko"
+
+
+//alert(myNameIs)
+//var text = promt("Напишите свой никнейм")
+//if (text === "Iko"){
+    //console.log("Верно");
+//else(text === "Beka"){
+    //console.log("Неверно"); 
+
+
+
+//var userDB = {
+    //name: "Iko",
+    //email:"iko09008@gmail.com",
+    //password: "qwerty123"
+//}
+
+
+
+var youPassword = prompt("Ведите пароль")
+if(youPassword === "qwerty123"){
+    console.log("Пароль правильный");
+}
+
+var youlogin = prompt("Ведите никнейм")
+if(youlogin === "iko"){
+    console.log("Welcome")
+}
+
